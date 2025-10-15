f47112053179d525e8ba699130a45fab1caf2e7a
Juna263 <240100797@aston.ac.uk>
2025-10-15 17:38:40 +0100

    declared its features

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python 3
+"""
+Password Strength Checker
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+
+It checks the strength of a password by:
+- Making rule-based checks and predictability
+- Checking whether it has appeared in any data leaks (Have I Been Pwned API)
+- Suggesting secure passphrases from a wordlist
+"""
+
